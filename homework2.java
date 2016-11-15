559ead9e3bd73459a40229572ba7205c979afffa
antoniten <ksusha123195@gmail.com>
2016-11-15 23:05:11 +0300

    Homeworkk

---
+++ homework2.java
@@ -0,0 +1,32 @@
+package Homeworks;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class homework2 {
+	 public static void main(String... args){
+	        char i = 0;
+	        int count = 0;
+	        HashMap<Integer, Character> words = new LinkedHashMap<>();
+	        for(i = '\u0430'; i < '\u044f'; i++){
+	            count++;
+	            words.put(count, i);
+	        }
+	        
+	        char[] line = "культурный презентационный материал предпосылки и развитие".toCharArray();
+	        for(int j = 0; j < line.length; j++){
+	            System.out.print(line[j] + "    ");
+	        }
+	        System.out.println("");
+	        for(int k = 0; k < line.length; k++){
+	            for(Entry<Integer, Character> abc: words.entrySet()){
+	                int key = abc.getKey();
+	                char value = abc.getValue();
+	                
+	                if(line[k] == value){
+	                    System.out.print(key + "");
+	                }
+	            }
+	        }
+	    }
+
+}
---
+++ homework_1.java
@@ -0,0 +1,31 @@
+package Homeworks;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class homework_1 {
+	 public static void main(String[] args) {
+	        System.out.print("Строка: ");
+	        Scanner a = new Scanner(System.in);
+	        String str = a.nextLine();
+	        
+	        System.out.print("Номер символа:");
+	        int k=a.nextInt();
+	        String p;
+	        String []words=str.split("[\\s\\.\\!\\,]");
+	        for(int l=0;l<words.length;l++){
+	            System.out.print("");
+	 
+	            if(k<words[l].length()){
+	                p="";
+	                p+=words[l].substring(0, k-1);
+	                p+='@';
+	                p+=words[l].substring(k,words[l].length());
+	 
+	                words[l]=p;
+	            }
+	            System.out.print(" "+words[l]);
+
+}
+	 }
+}
